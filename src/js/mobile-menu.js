d0e936693b026b175341f9f13a8995f17797711d
Max Sidorenko <fivemoshr@gmail.com>
2022-08-11 13:53:20 +0300

    add-start-setup

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,22 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+      mobileMenuRef.classList.toggle("is-open"); 
+  });
+    
+  // Close the mobile menu on wider screens if the device orientation changes
+     window.matchMedia('(min-width: 1199px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+    });
+})();
